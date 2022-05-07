7b430dced88e11293c04629b65e7bb4b6c16613a
lapinRagnar <lapinragnar@gmail.com>
2022-05-07 23:00:40 +0200

    generer composant pages

--- apps/mcdonald-clone/src/app/app.module.ts
+++ apps/mcdonald-clone/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, PagesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/mcdonald-clone/src/app/pages/pages.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mon-nx-monorepo-pages',
+  templateUrl: './pages.component.html',
+  styleUrls: ['./pages.component.scss'],
+})
+export class PagesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
