0b56e43c75d61e43a6087ba1a923408097294904
lapinRagnar <lapinragnar@gmail.com>
2022-05-07 23:10:19 +0200

    ajouter bootstrap > ng add @ng-bootstrap/ng-bootstrap

--- apps/mcdonald-clone/src/app/app.module.ts
+++ apps/mcdonald-clone/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { PagesComponent } from './pages/pages.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent, PagesComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
