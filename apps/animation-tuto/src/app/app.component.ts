86dddf20ac6783aac9fef4cbf51791b9ce92062f
lapinRagnar <lapinragnar@gmail.com>
2022-05-20 19:14:05 +0200

    animation stagger

--- apps/animation-tuto/src/app/app.component.ts
+++ apps/animation-tuto/src/app/app.component.ts
@@ -1,39 +1,87 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component } from '@angular/core';
 import {
   trigger,
   state,
   style,
   animate,
   transition,
+  query,
+  stagger,
+  keyframes,
   // ...
 } from '@angular/animations';
-import { fade } from './transitions/transition1';
+
+
 
 @Component({
   selector: 'mon-nx-monorepo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
-    trigger('openClose', [
-      // ...
-      state('open', style({
-        height: '200px',
-        opacity: 1,
-        backgroundColor: 'yellow'
-      })),
-      state('closed', style({
-        height: '100px',
-        opacity: 0.8,
-        backgroundColor: 'blue'
-      })),
-      transition('open <=> closed', [
-        animate('1s')
-      ]),
-      transition('closed <=> open', [
-        animate('0.5s')
-      ]),
-    ]),
-    fade
+    // trigger('openClose', [
+    //   // ...
+    //   state('open', style({
+    //     height: '200px',
+    //     opacity: 1,
+    //     backgroundColor: 'yellow'
+    //   })),
+    //   state('closed', style({
+    //     height: '100px',
+    //     opacity: 0.8,
+    //     backgroundColor: 'blue'
+    //   })),
+    //   transition('open <=> closed', [
+    //     animate('1s')
+    //   ]),
+    //   transition('closed <=> open', [
+    //     animate('0.5s')
+    //   ]),
+    // ]),
+    // fade,
+    trigger(
+      'listAnimation',
+      [
+        transition('* => *', [
+          query(':enter', style({opacity: 0}), {optional: true}),
+          query(
+            ':enter',
+            stagger(
+              '300ms',
+              [
+                animate(
+                  '1s ease-in',
+                  keyframes(
+                    [
+                      style({opacity: 0, transform: 'translateY(-75px)', offset: 0}),
+                      style({opacity: 0.5, transform: 'translateY(-35px)', offset: 0.3}),
+                      style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+                    ]
+                  )
+                )
+              ]),
+              {optional: true}
+            ),
+            query(
+              ':leave',
+              stagger(
+                '300ms',
+                [
+                  animate(
+                    '1s ease-in',
+                    keyframes(
+                      [
+                        style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+                        style({opacity: 0.5, transform: 'translateY(-35px)', offset: 0.3}),
+                        style({opacity: 0, transform: 'translateY(-75px)', offset: 1}),
+                      ]
+                    )
+                  )
+                ]),
+                {optional: true}
+            )
+        ])
+      ]
+    )
   ]
 })
 export class AppComponent {
