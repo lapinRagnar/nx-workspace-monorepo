6c99afc64d1b5d40ac00595b6a1147d509226679
lapinRagnar <lapinragnar@gmail.com>
2022-05-20 19:52:48 +0200

    explainer Animation

--- apps/animation-tuto/src/app/app.component.ts
+++ apps/animation-tuto/src/app/app.component.ts
@@ -81,6 +81,31 @@ import {
             )
         ])
       ]
+    ),
+
+    trigger(
+      'explainerAnim',
+      [
+        transition(
+          '* => *',
+          [
+            query('.col', style({opacity: 0, transform: 'translateX(-40px)'})),
+            query(
+              '.col',
+              stagger(
+                '500ms',
+                [
+                  animate(
+                    '800ms 1.2s ease-out',
+                    style({opacity: 1, transform: 'translateX(0)'})
+                    )
+                ]
+              )
+            )
+          ]
+        )
+      ]
+
     )
   ]
 })
