2d65a289273d601bb6ad0cde5968d5aac5ce2753
lapinRagnar <lapinragnar@gmail.com>
2022-05-20 15:01:52 +0200

    animation 1

--- apps/animation-tuto/src/app/app.module.ts
+++ apps/animation-tuto/src/app/app.module.ts
@@ -5,11 +5,14 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    BrowserAnimationsModule 
   ],
   providers: [],
   bootstrap: [AppComponent],
