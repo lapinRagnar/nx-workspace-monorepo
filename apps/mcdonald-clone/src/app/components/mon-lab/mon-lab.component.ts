90bafaa25862ea792f57ca5ce163716140ca2e4a
lapinRagnar <lapinragnar@gmail.com>
2022-05-22 09:36:31 +0200

    lesson 1 - dinding #template variable ref

--- apps/mcdonald-clone/src/app/components/mon-lab/mon-lab.component.ts
+++ apps/mcdonald-clone/src/app/components/mon-lab/mon-lab.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'mon-nx-monorepo-mon-lab',
   templateUrl: './mon-lab.component.html',
   styleUrls: ['./mon-lab.component.scss'],
 })
-export class MonLabComponent implements OnInit {
-  constructor() {}
+export class MonLabComponent  {
 
-  ngOnInit(): void {}
+
+  appelChiffre(chiffre: HTMLInputElement): void {
+    console.log(chiffre.value);
+    
+  }
 }
