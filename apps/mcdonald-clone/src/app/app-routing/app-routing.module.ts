f59682c3138c85cfcded0c37fe73fda6122c51d5
lapinRagnar <lapinragnar@gmail.com>
2022-05-22 18:10:26 +0200

    ajouter les routes

--- apps/mcdonald-clone/src/app/app-routing/app-routing.module.ts
+++ apps/mcdonald-clone/src/app/app-routing/app-routing.module.ts
@@ -6,13 +6,17 @@ import { LesBigQuestionsComponent } from './../components/pages/body/les-big-que
 import { BodyComponent } from './../components/pages/body/body.component';
 import { EnFamilleComponent } from './../components/pages/body/en-famille/en-famille.component';
 import { NosActualitesComponent } from './../components/pages/body/nos-actualites/nos-actualites.component';
+import { LoginComponent } from '../components/authentication/login/login.component';
+import { RegisterComponent } from '../components/authentication/register/register.component';
 
 
 const routes: Routes = [
   {path: '', component: BodyComponent},
   {path: 'les-big-questions', component: LesBigQuestionsComponent},
   {path: 'en-famille', component: EnFamilleComponent},
-  {path: 'nos-actualites', component: NosActualitesComponent}
+  {path: 'nos-actualites', component: NosActualitesComponent},
+  {path: 'connexion', component: LoginComponent},
+  {path: 'inscription', component: RegisterComponent},
 ]
 
 @NgModule({
