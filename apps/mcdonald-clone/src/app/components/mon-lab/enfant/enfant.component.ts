3038a27a8079e70b44d3551102aa92b7a1383910
lapinRagnar <lapinragnar@gmail.com>
2022-05-22 15:54:20 +0200

    lesson  @Output - bind enfant vers parent

--- apps/mcdonald-clone/src/app/components/mon-lab/enfant/enfant.component.ts
+++ apps/mcdonald-clone/src/app/components/mon-lab/enfant/enfant.component.ts
@@ -1,18 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'mon-nx-monorepo-enfant',
   template: ` 
 
-    <p>dans l'enfant: {{ item }} </p>
+    <p>vers dans l'enfant: {{ item }} </p>
+    <button (click)="addNewItem(value)" >Evoyer au parent</button>
     
     `,
   styles: [],
 })
 export class EnfantComponent  {
 
+  value = 'comment ca va'
 
   @Input() item: string
+
+  @Output() newEventEmitter = new EventEmitter<string>();
+
+
+  addNewItem(value: string) {
+    this.newEventEmitter.emit(value)
+  }
  
   direBonjour() {
     console.log("bonjour");    
--- apps/mcdonald-clone/src/app/components/mon-lab/parent/parent.component.ts
+++ apps/mcdonald-clone/src/app/components/mon-lab/parent/parent.component.ts
@@ -4,15 +4,23 @@ import { Component } from '@angular/core';
   selector: 'mon-nx-monorepo-parent',
   template: ` 
     <hr>
-    <p>Dans le parent : {{ parentVariable }} </p>
+    <p>Du le parent : {{ parentVariable }} </p>
+    <p> donnée de l'enfant {{deLenfant}} </p>
 
     <!-- <p>variable de l'enfant : {{ comment_ca_va }} </p> -->
-    <mon-nx-monorepo-enfant [item]="parentVariable" ></mon-nx-monorepo-enfant> 
+    <mon-nx-monorepo-enfant 
+      [item]="parentVariable" 
+      (newEventEmitter)="addItem($event)"
+      >
+    </mon-nx-monorepo-enfant> 
   `,
   styles: [],
 })
 export class ParentComponent {
-
+  deLenfant = ''
   parentVariable = "Bonjour avec @Input"  // variable à exporter dans l'enfant
 
+  addItem(newItem: string){
+    this.deLenfant = newItem
+  }
 }
